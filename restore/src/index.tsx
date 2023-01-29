36e734799eb1c96ab35d9898f1e044c23456b5c0
shahin1290 <s.patowary@yahoo.com>
2023-01-29 20:44:40 +0100

    sets up redux store

--- restore/src/index.tsx
+++ restore/src/index.tsx
@@ -4,6 +4,8 @@ import App from "./app/layout/App";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { StoreProvider } from "./app/context/StoreContext";
+import { Provider } from "react-redux";
+import { store } from "./app/store/configureStore";
 
 export const history = createBrowserHistory();
 
@@ -11,7 +13,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
       <StoreProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </StoreProvider>
     </Router>
   </React.StrictMode>,
